e9bfd8ff12206e712866934cbce6ccb1bb3f817b
tarasovsy <semyon.tarasov.02@list.ru>
2024-12-08 19:54:37 +0300

    Add DAG, receive data from an open IP address and upload it to Postgres

--- dags/get_IP_to_save_postgres.py
+++ dags/get_IP_to_save_postgres.py
@@ -1,121 +1,62 @@
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
-from datetime import datetime
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from datetime import datetime, timedelta
+from airflow.decorators import task_group  # Импорт декоратора task_group
 import requests
 import json
 
-default_args = {
-    'start_date': datetime(2024, 11, 22),
-}
-
-dag = DAG(
-    'get_IP_to_save_postgres',
-    default_args=default_args,
-    schedule_interval='@hourly',
-)
-
-
 # Функция для выполнения запроса к API
 def fetch_weather_data():
-    url = 'http://api.weatherstack.com/current'
-    params = {
-        'access_key': 'ab95de45fc66b434ec704f9dd0149bbd',
-        'query': 'Moscow',
-        'units': 'm'
-    }
-    response = requests.get(url, params=params)
-
+    url = 'https://jsonplaceholder.typicode.com/posts'
+    response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        if "current" in data:  # Проверяем, что ключ "current" присутствует
-            return data
-        else:
-            raise ValueError("Unexpected API response structure: 'current' data missing.")
+        print(f"Fetched data: {json.dumps(data[:5], indent=2)}")  # Выводим первые 5 записей для проверки
+        return data  # Данные будут сохранены в XCom
     else:
         raise Exception(f"API request failed with status code {response.status_code}")
 
 
-# Функция для парсинга JSON и создания SQL-запроса
-def parse_and_prepare_sql(**context):
-    response = context['ti'].xcom_pull(task_ids='fetch_weather_data')
+# Функция для загрузки данных в таблицу PostgreSQL
+def save_data_to_postgres(ti):
+    # Получаем данные из XCom
+    data = ti.xcom_pull(task_ids='fetch_data_task')
+    if not data:
+        raise ValueError("No data fetched from API to save in PostgreSQL")
 
-    data = {
-        "request_type": response["request"]["type"],
-        "query": response["request"]["query"],
-        "language": response["request"]["language"],
-        "unit": response["request"]["unit"],
-        "location_name": response["location"]["name"],
-        "location_country": response["location"]["country"],
-        "location_region": response["location"]["region"],
-        "location_lat": response["location"]["lat"],
-        "location_lon": response["location"]["lon"],
-        "location_timezone_id": response["location"]["timezone_id"],
-        "location_localtime": response["location"]["localtime"],
-        "location_localtime_epoch": response["location"]["localtime_epoch"],
-        "location_utc_offset": response["location"]["utc_offset"],
-        "observation_time": response["current"]["observation_time"],
-        "temperature": response["current"]["temperature"],
-        "weather_code": response["current"]["weather_code"],
-        "weather_icon": response["current"]["weather_icons"][0],
-        "weather_description": response["current"]["weather_descriptions"][0],
-        "wind_speed": response["current"]["wind_speed"],
-        "wind_degree": response["current"]["wind_degree"],
-        "wind_dir": response["current"]["wind_dir"],
-        "pressure": response["current"]["pressure"],
-        "precip": response["current"]["precip"],
-        "humidity": response["current"]["humidity"],
-        "cloudcover": response["current"]["cloudcover"],
-        "feelslike": response["current"]["feelslike"],
-        "uv_index": response["current"]["uv_index"],
-        "visibility": response["current"]["visibility"],
-        "is_day": True if response["current"]["is_day"] == 'yes' else False
-    }
-
-    # SQL-запрос для вставки данных
-    insert_query = f"""
-    INSERT INTO "IAD".weather_data (
-        request_type, query, language, unit, location_name, location_country, location_region,
-        location_lat, location_lon, location_timezone_id, location_localtime, location_localtime_epoch,
-        location_utc_offset, observation_time, temperature, weather_code, weather_icon,
-        weather_description, wind_speed, wind_degree, wind_dir, pressure, precip, humidity,
-        cloudcover, feelslike, uv_index, visibility, is_day
-    ) VALUES (
-        '{data["request_type"]}', '{data["query"]}', '{data["language"]}', '{data["unit"]}', 
-        '{data["location_name"]}', '{data["location_country"]}', '{data["location_region"]}',
-        {data["location_lat"]}, {data["location_lon"]}, '{data["location_timezone_id"]}', 
-        '{data["location_localtime"]}', {data["location_localtime_epoch"]}, {data["location_utc_offset"]},
-        '{data["observation_time"]}', {data["temperature"]}, {data["weather_code"]}, '{data["weather_icon"]}',
-        '{data["weather_description"]}', {data["wind_speed"]}, {data["wind_degree"]}, '{data["wind_dir"]}', 
-        {data["pressure"]}, {data["precip"]}, {data["humidity"]}, {data["cloudcover"]}, 
-        {data["feelslike"]}, {data["uv_index"]}, {data["visibility"]}, {data["is_day"]}
-    );
+    # Подключение к PostgreSQL через PostgresHook
+    pg_hook = PostgresHook(postgres_conn_id='postgres')  # ID подключения к БД в Airflow
+    insert_query = """
+        INSERT INTO \"IAD\".posts (user_id, post_id, title, body)
+        VALUES (%s, %s, %s, %s);
     """
-    return insert_query
 
+    # Вставка записей в базу
+    conn = pg_hook.get_conn()
+    cursor = conn.cursor()
+    for record in data:
+        cursor.execute(insert_query, (
+            record['userId'], record['id'], record['title'], record['body']
+        ))
+    conn.commit()
+    print("Data successfully saved to PostgreSQL")
+    
 
-# Задача для получения данных
-fetch_weather_data_task = PythonOperator(
-    task_id='fetch_weather_data',
-    python_callable=fetch_weather_data,
-    dag=dag,
-)
-
-# Задача для подготовки SQL
-generate_sql_task = PythonOperator(
-    task_id='generate_sql',
-    python_callable=parse_and_prepare_sql,
-    provide_context=True,
-    dag=dag,
-)
-
-# Задача для вставки данных
-insert_data_task = PostgresOperator(
-    task_id='insert_data',
-    postgres_conn_id='postgres',
-    sql="{{ task_instance.xcom_pull(task_ids='generate_sql') }}",
-    dag=dag,
-)
-
-# Последовательность выполнения задач
-fetch_weather_data_task >> generate_sql_task >> insert_data_task
+# Создание DAG
+with DAG(
+    dag_id='get_IP_to_save_postgres',
+    start_date=datetime(2024, 11, 1),
+    schedule_interval=None,  # Запуск вручную
+    catchup=False,
+) as dag:
+    fetch_data = PythonOperator(
+        task_id='fetch_data_task',
+        python_callable=fetch_weather_data
+    )
+    save_data = PythonOperator(
+        task_id='save_data_to_postgres_task',
+        python_callable=save_data_to_postgres
+    )
+    # Упорядочивание задач
+    fetch_data >> save_data
